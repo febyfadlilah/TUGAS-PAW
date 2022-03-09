1d7a25304d7eb4d39630711b9a889ebd07d05ada
febyfadlilah <febyfadlilah98@gmail.com>
2022-03-09 19:13:53 +0700

    Menambahkan file variable yang berisi penjumlahan,pengurangan,dan perkalian

---
+++ variable.php
@@ -0,0 +1,12 @@
+<?php
+//membuat variable baru
+$angka1 = 5 ; 
+$angka2 = 9 ;
+$angka3 = 4 ;
+//menampilkan operasi dengan menggunakan nilai yang ada didalam variable
+echo ("Hasil dari $angka1 + $angka3 = ".$angka1+$angka3."<br>");
+echo ("Hasil dari $angka1 + $angka2 + $angka3 = ".$angka1+$angka2+$angka3."<br>");
+echo ("Hasil dari $angka2 - $angka1 = ".$angka2-$angka1."<br>");
+echo ("Hasil dari $angka2 - $angka1-$angka3 = ".$angka2-$angka1-$angka3."<br>");
+echo ("Hasil dari $angka1 x $angka2 x $angka3 = ".$angka1*$angka2*$angka3)
+?>
